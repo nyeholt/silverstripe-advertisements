24160a0ff36fc21c98cfe01c70d815e0e4c55ca4
Simon Wade <simon.wade@gmail.com>
2011-06-16 08:20:15 +1000

    Fixed incorrect named subpattern syntax in preg_match

--- code/dataobjects/AdImpression.php
+++ code/dataobjects/AdImpression.php
@@ -105,8 +105,8 @@ function get_browser_info()
     
     // finally get the correct version number
     $known = array('Version', $ub, 'other');
-    $pattern = '#(?<browser>' . join('|', $known) .
-    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    $pattern = '#(?P<browser>' . join('|', $known) .
+    ')[/ ]+(?P<version>[0-9.|a-zA-Z.]*)#';
     if (!preg_match_all($pattern, $u_agent, $matches)) {
         // we have no matching number just continue
     }
